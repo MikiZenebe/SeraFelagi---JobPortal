c5da8f2915095c735072feacf06ac72245bee987
MikiZenebe <mikizenebe10gmail.com>
2024-11-01 17:44:46 +0300

    sign

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <Button>Click Me</Button>
+
+      <UserButton />
     </div>
   );
 }
